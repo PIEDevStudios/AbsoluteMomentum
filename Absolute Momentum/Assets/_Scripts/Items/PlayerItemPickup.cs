c9a8d3e3c35cabf759a4032650f8a3b1144b9ab2
KALE <114507504+kaleyvuong@users.noreply.github.com>
2025-02-09 21:02:29 -0800

    PlayerItemPickup fixed question mark?

--- Absolute Momentum/Assets/_Scripts/Items/PlayerItemPickup.cs
+++ Absolute Momentum/Assets/_Scripts/Items/PlayerItemPickup.cs
@@ -1,26 +1,20 @@
 using UnityEngine;
-
-[CreateAssetMenu(fileName = "NewScriptableObjectScript", menuName = "Scriptable Objects/NewScriptableObjectScript")]
-public class NewScriptableObjectScript : ScriptableObject
+using Random = System.Random;
+public class PlayerItemPickup : MonoBehaviour
 {
     [SerializeField] private ItemPoolSO itemPool;
 
-    Update() {
+    void Update() {
         if (Input.GetKeyDown(KeyCode.E))
         {
-            SelectItem()
-        }
-
+            SelectItem();
+        }   
     }
-}
-
-
-
-private void SelectItem()
-{
-    Random rnd = new Random();
-
-    int randomItemNum = rnd.Next(ItemPoolSO.items.Length);
-
-    Instantiate(ItemPoolSO[randomItemNum])
+    private void SelectItem()
+    {
+        Random rnd = new Random();  
+        int randomItemNum = rnd.Next(itemPool.items.Length);
+        ItemPickupSO selectedItem = itemPool.items[randomItemNum];
+        Instantiate(selectedItem.powerup);
+    }
 }
