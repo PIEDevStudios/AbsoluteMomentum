cab486dd57053af58ba95769a9e0d5d9966e19e3
acabochan <166557187+acabochan@users.noreply.github.com>
2025-03-09 21:40:34 -0400

    race countdown

---
+++ Absolute Momentum/Assets/_Scripts/UI/RaceCountdownManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using UnityEngine;
+using TMPro;
+using System; 
+
+public class RaceCountdownManager : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI countdownText; // Reference to TextMeshProUGUI
+    public event Action OnCountdownFinished; // Event triggered when countdown finishes
+
+    private void Start()
+    {
+        countdownText.gameObject.SetActive(false); // Hide initially
+    }
+
+    public void StartCountdown()
+    {
+        StartCoroutine(CountdownCoroutine());
+    }
+
+    private IEnumerator CountdownCoroutine()
+    {
+        countdownText.gameObject.SetActive(true); // Show the text
+
+        int countdown = 3;
+        while (countdown > 0)
+        {
+            countdownText.text = countdown.ToString();
+            yield return new WaitForSeconds(1f);
+            countdown--;
+        }
+
+        countdownText.text = "GO!";
+        yield return new WaitForSeconds(1f);
+
+        countdownText.gameObject.SetActive(false); // Hide after countdown
+
+        OnCountdownFinished?.Invoke(); // Invoke the event
+    }
+}
