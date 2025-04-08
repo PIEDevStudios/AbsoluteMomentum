5c74004595069ec975892ef4b85e448d9e4d1856
acabochan <166557187+acabochan@users.noreply.github.com>
2025-04-08 19:47:01 -0400

    Disabling player input

--- Absolute Momentum/Assets/_Scripts/UI/RaceCountdownManager.cs
+++ Absolute Momentum/Assets/_Scripts/UI/RaceCountdownManager.cs
@@ -1,28 +1,32 @@
 using System.Collections;
 using UnityEngine;
 using TMPro;
-using System; 
+using System;
+using System.Linq;
 
 public class RaceCountdownManager : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI countdownText; // Reference to TextMeshProUGUI
     public event Action OnCountdownFinished; // Event triggered when countdown finishes
 
+    [SerializeField] private Player[] players; // Reference to all players
+
     private void Start()
     {
         countdownText.gameObject.SetActive(false);
     }
-    
+
     public void StartCountdown()
     {
+        // Disable player input before countdown starts
+        DisablePlayerInput();
+
         countdownText.gameObject.SetActive(true);
         StartCoroutine(CountdownCoroutine());
     }
 
     private IEnumerator CountdownCoroutine()
     {
-        countdownText.gameObject.SetActive(true); // Show the text
-
         int countdown = 3;
         while (countdown > 0)
         {
@@ -36,6 +40,27 @@ public class RaceCountdownManager : MonoBehaviour
 
         countdownText.gameObject.SetActive(false); // Hide after countdown
 
+        // Enable player input after countdown finishes
+        EnablePlayerInput();
+
         OnCountdownFinished?.Invoke(); // Invoke the event
+    }
+
+    // Method to disable player input
+    private void DisablePlayerInput()
+    {
+        foreach (var player in players)
+        {
+            player.GetComponent<PlayerInput>().enabled = false; // Disable input on each player
+        }
+    }
+
+    // Method to enable player input after the countdown finishes
+    private void EnablePlayerInput()
+    {
+        foreach (var player in players)
+        {
+            player.GetComponent<PlayerInput>().enabled = true; // Enable input on each player
+        }
     }
 }
