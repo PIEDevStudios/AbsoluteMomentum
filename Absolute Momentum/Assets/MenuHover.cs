37daa1b0a8a746bb2ed9335065c238e6ee17f7a9
Plutzz <benjiongco@gmail.com>
2025-06-25 19:48:19 -0700

    DOTween Update

--- Absolute Momentum/Assets/MenuHover.cs
+++ Absolute Momentum/Assets/MenuHover.cs
@@ -1,17 +1,23 @@
 using UnityEngine;
-
+using DG.Tweening;
 public class MenuHover : MonoBehaviour {
     public float hoverAmount = 5f;
     public float hoverSpeed = 2f;
-
+    public Ease ease;
+    public RectTransform rectTransform;
     private Vector3 startPos;
+    
 
     void Start() {
-        startPos = transform.localPosition;
+        startPos = rectTransform.localPosition;
+        
+        rectTransform.DOLocalMoveY(startPos.y + hoverAmount, hoverSpeed).SetEase(ease).SetLoops(-1, LoopType.Yoyo);
+        
+        
     }
 
     void Update() {
-        float offset = Mathf.Sin(Time.time * hoverSpeed) * hoverAmount;
-        transform.localPosition = startPos + new Vector3(0, offset, 0);
+        // float offset = Mathf.Sin(Time.time * hoverSpeed) * hoverAmount;
+        // transform.localPosition = startPos + new Vector3(0, offset, 0);
     }
 }
